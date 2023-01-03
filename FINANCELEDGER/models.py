7f2093548396ef162e19dedcf39acd787a23574d
kyounghoon <kkh2826@naver.com>
2023-01-03 11:52:45 +0900

    신규 Model FinanceLedgerList 추가

--- FINANCELEDGER/models.py
+++ FINANCELEDGER/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Financeledgerlist(models.Model):
+    stddate = models.DateField(db_column='stdDate', primary_key=True)  # Field name made lowercase.
+    email = models.CharField(max_length=50)
+    seq = models.IntegerField()
+    amount = models.BigIntegerField()
+    memo = models.CharField(max_length=50, blank=True, null=True)
+    paytype = models.IntegerField(db_column='payType')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createDate')  # Field name made lowercase.
+    updatedate = models.DateTimeField(db_column='updateDate')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'financeledgerlist'
+        unique_together = (('stddate', 'email', 'seq'),)
